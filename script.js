ea2d03a18eda46ac60f62e933757697486c8fbee
gehrmansSparrow <gehrmans25@gmail.com>
2026-09-06 14:52:51 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-// The page intentionally uses normal browser scrolling.
-// On an iPhone, swiping up/down will scroll this page naturally just like the reference.
-document.querySelectorAll('a[href="#"]').forEach((link) => {
-  link.addEventListener('click', (event) => event.preventDefault());
-});
